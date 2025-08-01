9634814b9dbf7efc202439491258281fc93d7064
Gundam Indu <gundamindu@Gundams-MacBook-Air.local>
2025-08-01 11:16:00 +0530

    Add environment variables to .env
    Signed-off-by: Gundam Indu <gundamindu@Gundams-MacBook-Air.local>

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-    cloud_name: "dzgeyeyto",
-    api_key: "284568567968357",
-    api_secret: "XXbAXGqfS7bLed9mbb5DJ6rOg1U",
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret,
 });
 
 export default cloudinary;
