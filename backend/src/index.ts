05dba92283d412f0b065629599b9813466c28ed6
indugundam <indu.g@inncircles.com>
2025-08-25 17:05:44 +0530

    Final commit
    Signed-off-by: indugundam <indu.g@inncircles.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,3 +26,4 @@ connectDB().then(() => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 });
+
---
+++ frontend/helper_module/lib/screens/helper_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HelperList extends StatefulWidget {
+  const HelperList({super.key});
+
+  @override
+  State<HelperList> createState() => _HelperListState();
+}
+
+class _HelperListState extends State<HelperList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
